c8dc821a230366be25d7b3397df0e6a335b0cd64
beardm4n <dmitriykorovko@gmail.com>
2022-02-14 19:12:16 +0300

    fix accordion in seo block

--- src/components/seo.svelte
+++ src/components/seo.svelte
@@ -5,9 +5,17 @@
 		{/each}
 	</div>
 
-	<div class="seo_info">
-		<div class="info_title">Скучный SEO текст</div>
-	</div>
+	{#each clicked as acc, idx}
+		<div class="info" class:info_open={clicked[idx]} on:click={() => clickAccordion(idx)}>
+			<div class="info_title">Через какое время выполняются услуги?</div>
+
+			<img class="arrow" class:rotate={clicked[idx]} src="img/svg/accordion_arrow.svg" alt="arrow">
+		</div>
+
+		{#if clicked[idx]}
+			<div class="more_info">Для заказа выберите услугу, введите ссылку и укажите количество. Вас перенаправит на сайт платежной системы, где нужно будет произвести оплату. После совершения оплаты Ваш заказ автоматически начнет выполняться.</div>
+		{/if}
+	{/each}
 
 	<div class="seo_btn">
 		<button>
@@ -19,6 +27,12 @@
 
 <script>
 	import SeoCard from '../components/items/seo-card.svelte'
+
+	let clicked = [false]
+
+	function clickAccordion(idx) {
+		clicked[idx] = clicked[idx] ? !clicked[idx] : true
+	}
 </script>
 
 <style lang="sass">
@@ -31,19 +45,60 @@
 			display: flex
 			flex-wrap: wrap
 
-		&_info
-			min-height: 51px
+		.info
+			width: $container_width
+			position: relative
+			min-height: 50px
 			display: flex
 			flex-direction: column
 			justify-content: center
 			align-items: flex-start
 			box-shadow: 0 0 70px rgba(0, 0, 0, 0.1)
 			border-radius: 10px
-			margin-top: 15px
-			margin-bottom: 15px
+			margin-bottom: 14px
 			background: #fff
-			color: #263238
-			padding: 0 40px
+			font-weight: 500
+			font-size: 18px
+			color: #000
+			margin-top: 25px
+
+			&_open
+				background: #7EB0FF
+				margin-bottom: 0
+				border-radius: 10px 10px 0 0
+				color: #fff
+				font-size: 18px
+				font-weight: 700
+
+				&:hover
+					background: #7EB0FF !important
+
+			&_title
+				padding: 15px 30px
+
+			&:hover
+				cursor: pointer
+				background: #ecf5ff
+
+			.arrow
+				position: absolute
+				top: 50%
+				right: 30px
+				transition: $transition-slow
+
+			.rotate
+				transform: rotate(180deg)
+				transition: $transition-slow
+
+		.more_info
+			padding: 15px 50px
+			background: #FFFFFF
+			border-radius: 0 0 10px 10px
+			box-shadow: 0 0 70px rgb(0 0 0 / 10%)
+			margin-bottom: 14px
+			font-weight: 400
+			font-size: 18px
+			color: #8499A4
 
 		&_btn
 			display: flex
